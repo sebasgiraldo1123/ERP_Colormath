0ac9f3ad42165dd4040facaa4f854e1bc56e19d2
sebasgiraldo1123 <sebasgiraldo1123@gmail.com>
2023-08-09 12:34:46 -0500

    Se finaliza el módulo de pedidos

--- __Orders.js
+++ __Orders.js
@@ -9,30 +9,30 @@ function filterOrders(parameter) {
   // Time
   //var startTime = new Date();
 
-  const orders = [];
+  let orders = null;
 
   switch (parameter) {
     case "POR HACER":
-      searchOrdersByParameter("POR HACER", orders);
+      orders = searchOrdersByParameter("POR HACER");
       break;
     case "ENTREGADO":
-      searchOrdersByParameter("ENTREGADO", orders);
+      orders = searchOrdersByParameter("ENTREGADO");
       break;
     case "CANCELADO":
-      searchOrdersByParameter("CANCELADO", orders);
+      orders = searchOrdersByParameter("CANCELADO");
       break;
     case "LISTO":
-      searchOrdersByParameter("LISTO", orders);
+      orders = searchOrdersByParameter("LISTO");
       break;
     case "DISEÑADO":
-      searchOrdersByParameter("DISEÑADO", orders);
+      orders = searchOrdersByParameter("DISEÑADO");
       break;
     case "IMPRESO":
-      searchOrdersByParameter("IMPRESO", orders);
+      orders = searchOrdersByParameter("IMPRESO");
       break;
     default:
-      searchOrdersByParameter("LISTO", orders);
-      searchOrdersByParameter("POR HACER", orders);
+      orders = searchOrdersByParameter("LISTO");
+      orders = orders.concat(searchOrdersByParameter("POR HACER"));
       break;
   }
 
@@ -44,16 +44,17 @@ function filterOrders(parameter) {
     return null;
   }
   else {
-    return orders;
+    // Ordena los elementos del vector de acuerdo a la última fila, es decir, el plazo
+    return orders.sort((a, b) => a[a.length - 1] - b[b.length - 1]);
   }
-
 }
 
 /**
  * Encuentra todas las ordenes de un estado en específico y las agrega al vector sale
  */
-function searchOrdersByParameter(searchText, orders) {
+function searchOrdersByParameter(searchText) {
 
+  let orders = [];
   const range = "A2:H";
   let rowList = [];
 
@@ -65,8 +66,46 @@ function searchOrdersByParameter(searchText, orders) {
   // Se crea un objeto con los datos de las filas proporcionadas
   for (let i = 0; i < rowList.length; i++) {
     orders.push(PEDIDOS_TABLE.getRange(rowList[i].getRow(), 1, 1, PEDIDOS_TABLE.getLastColumn()).getValues()[0]);
+
+    if (searchText !== "ENTREGADO" && searchText !== "CANCELADO") {
+      // Calcula el plazo con la diferencia entre la fecha actual y la fecha de entrega, lo agrega en la última fila del vector
+      orders[i].push((Math.floor((new Date(orders[i][5]) - new Date()) / (1000 * 60 * 60 * 24))) + 1);
+    }
   }
+
+  return orders;
 }
+
+
+/*
+ * retorna la cantidad de entregas que hay para un dia en específico
+ */
+function getOrdersByDate(date) {
+
+  const range = "F2:F";
+  let rowList = [];
+  let countOrders = 0;
+
+  const textFinder = PEDIDOS_TABLE.getRange(range)
+    .createTextFinder(date).matchEntireCell(true);
+
+  rowList = textFinder.findAll();
+
+  for (let i = 0; i < rowList.length; i++) {
+    let state = PEDIDOS_TABLE.getRange(rowList[i].getRow(), 2).getValue();
+    if (state === "POR HACER") {
+      countOrders++;
+    }
+  }
+  return countOrders;
+}
+
+
+
+
+
+
+
 
 
 
